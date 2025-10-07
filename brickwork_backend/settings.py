c28b29d9463a41b63d518126b8b475f3658065e0
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2025-10-07 19:59:45 +0100

    added static file configurations

--- brickwork_backend/settings.py
+++ brickwork_backend/settings.py
@@ -121,8 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Optional: Add this if you have static files in your apps
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
