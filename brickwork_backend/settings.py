448be19f141a619489fa7d569804afb39e25c438
DANIEL-EKWERE <102237731+DANIEL-EKWERE@users.noreply.github.com>
2025-10-08 12:44:09 +0100

    Update settings.py

--- brickwork_backend/settings.py
+++ brickwork_backend/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOG_DIR = os.path.join(BASE_DIR, "logs")
+BASE_DIRx = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_DIR = os.path.join(BASE_DIRx, "logs")
 LOG_FILE = os.path.join(LOG_DIR, "ingestion.log")
 
 os.makedirs(LOG_DIR, exist_ok=True)
